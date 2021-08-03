3ba83ac48bf51a7d6532988f825a8483904b8364
punit008 <punitwork18@gmail.com>
2021-08-03 08:53:35 +0530

    blade component

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layout')
 
-    {{-- Custom Css --}}
-    <link rel="stylesheet" href="/app.css">
-    {{-- Custom javascript --}}
-    <script src="app.js"></script>
-    <title>My Blog</title>
-</head>
-<body>
-
+@section('content')
     <article>
-        <h1> <?php echo $post->title; ?></h1>
+        <h1>{{ $post->title }}</h1>
         <div>
-            <?php echo $post->body; ?>
+            {!! $post->body !!}
         </div>
     </article>
 
     <a href="/">Go back</a>
-</body>
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,31 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<x-layout>
 
-    {{-- Custom Css --}}
-    <link rel="stylesheet" href="/app.css">
-    {{-- Custom javascript --}}
-    <script src="app.js"></script>
-    <title>My Blog</title>
-</head>
-<body>
-     <?php foreach($posts as $post): ?>
+        @foreach ($posts as $post)
 
-    <article>
-       <h1>
-           
-           <a href="/post/<?php echo $post->slug; ?>"><?php echo $post->title; ?></a>
-           
-        </h1> 
-       <div>
-           <?php echo $post->excerpt?>
-       </div>
-    </article>
+            <article>
+                <h1>
 
-    <?php endforeach; ?>
+                    <a href="/post/{{ $post->slug }} ">{{ $post->title }} </a>
 
-</body>
-</html>
+                </h1>
+                <div>
+                    {{ $post->excerpt }}
+                </div>
+            </article>
+
+        @endforeach
+ 
+</x-layout>
