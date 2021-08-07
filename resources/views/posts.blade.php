f64de983ba1330555322f59f9cb7116644211290
punit008 <punitwork18@gmail.com>
2021-08-07 17:12:03 +0530

    add more component for posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-        @foreach ($posts as $post)
+    {{-- @foreach ($posts as $post)
 
             <article>
                 <h1>
@@ -19,6 +19,24 @@
                 </div>
             </article>
 
-        @endforeach
- 
+        @endforeach --}}
+
+    @include('_post-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        <x-post-featured-card></x-post-featured-card>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card></x-post-card>
+            <x-post-card></x-post-card>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card></x-post-card>
+            <x-post-card></x-post-card>
+            <x-post-card></x-post-card>
+        </div>
+    </main>
+
 </x-layout>
