8234f9f1ad1fc3fc31047fee61157754655fef35
Punit <punitwork18@gmail.com>
2021-08-23 22:38:26 +0530

    added comment factory, Controller and migration

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,18 @@
+
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div>
+        <img src="https://i.pravatar.cc/100" alt="">
+    </div>
+    <div>
+        <header>
+            <h3 class="font-bold">John Doe</h3>
+            <p>
+                Posted
+                <time> 8 months ago </time>
+            </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi 
+        </p>
+    </div>
+</article>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -42,14 +42,18 @@
                     </div>
 
                     <h1 class="font-bold text-3xl lg:text-4xl mb-10">
-                       {{ $post->title }}
+                        {{ $post->title }}
                     </h1>
 
                     <div class="space-y-4 lg:text-lg leading-loose">
                         <p>{!! $post->body !!}</p>
                     </div>
                 </div>
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-post-comment />
+                    <x-post-comment />
+                </section>
             </article>
-        </main> 
+        </main>
     </section>
 </x-layout>
